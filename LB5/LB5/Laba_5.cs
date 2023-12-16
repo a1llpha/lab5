357ace3b29216c2cb6c74fb32b3f8bebcbf58fa8
a1llpha <146925733+a1llpha@users.noreply.github.com>
2023-12-17 00:17:05 +0300

    Add files via upload

---
+++ LB5/LB5/Laba_5.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public abstract class Vehicle
+{
+    public int Speed { get; set; }
+    public int Capacity { get; set; }
+
+    public abstract void Move();
+}
+
+
+public class Human
+{
+    public int Speed { get; set; }
+
+    public void Move()
+    {
+        Console.WriteLine("Human is moving.");
+    }
+}
+
+
+public class Car : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Car is moving.");
+    }
+}
+
+public class Bus : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Bus is moving.");
+    }
+}
+
+public class Train : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Train is moving.");
+    }
+}
+
+
+public class Route
+{
+    public string StartPoint { get; set; }
+    public string EndPoint { get; set; }
+}
+
+
+public class TransportNetwork
+{
+    private List<Vehicle> vehicles;
+
+    public TransportNetwork()
+    {
+        vehicles = new List<Vehicle>();
+    }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        vehicles.Add(vehicle);
+    }
+
+    public void ControlMovement()
+    {
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.Move();
+        }
+    }
+
+    public string CalculateOptimalRoute(Route route, Vehicle vehicle)
+    {
+        return $"Optimal route from {route.StartPoint} to {route.EndPoint} for {vehicle.GetType().Name}";
+    }
+
+    public void PassengerHandling(Vehicle vehicle, int passengers)
+    {
+        Console.WriteLine($"Handling {passengers} passengers for {vehicle.GetType().Name}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car car = new Car();
+        Bus bus = new Bus();
+        Train train = new Train();
+
+        TransportNetwork transportNetwork = new TransportNetwork();
+        transportNetwork.AddVehicle(car);
+        transportNetwork.AddVehicle(bus);
+        transportNetwork.AddVehicle(train);
+
+        transportNetwork.ControlMovement();
+
+        Route route = new Route { StartPoint = "A", EndPoint = "B" };
+        Console.WriteLine(transportNetwork.CalculateOptimalRoute(route, car));
+
+        transportNetwork.PassengerHandling(bus, 30);
+    }
+}
---
+++ LB5_2/LB5_2/LB5_2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class MathOperations
+{
+   
+    public static T Add<T>(T a, T b)
+    {
+        dynamic dynamicA = a;
+        dynamic dynamicB = b;
+        return dynamicA + dynamicB;
+    }
+
+л
+    public static T[] Add<T>(T[] a, T[] b)
+    {
+        if (a.Length != b.Length)
+            throw new ArgumentException("Arrays must have the same length.");
+
+        T[] result = new T[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            result[i] = Add(a[i], b[i]);
+        }
+        return result;
+    }
+
+ 
+    public static T[,] Add<T>(T[,] matrixA, T[,] matrixB)
+    {
+        int rows = matrixA.GetLength(0);
+        int columns = matrixA.GetLength(1);
+
+        if (rows != matrixB.GetLength(0) || columns != matrixB.GetLength(1))
+            throw new ArgumentException("Matrices must have the same dimensions.");
+
+        T[,] result = new T[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                result[i, j] = Add(matrixA[i, j], matrixB[i, j]);
+            }
+        }
+        return result;
+    }
+
+   
+    public static void Main()
+    {
+        int a = 5, b = 10;
+        Console.WriteLine($"Addition of {a} and {b}: {Add(a, b)}");
+
+        double[] arrayA = { 1.5, 2.5, 3.5 };
+        double[] arrayB = { 0.5, 1.5, 2.5 };
+        Console.WriteLine($"Array Addition: [{string.Join(", ", Add(arrayA, arrayB))}]");
+
+        int[,] matrixA = { { 1, 2 }, { 3, 4 } };
+        int[,] matrixB = { { 5, 6 }, { 7, 8 } };
+        var resultMatrix = Add(matrixA, matrixB);
+
+        Console.WriteLine("Matrix Addition:");
+        for (int i = 0; i < resultMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                Console.Write(resultMatrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
